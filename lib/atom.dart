70d48dc4b855d249ad468f7bb30359a1ab6a7bdc
Gabriel Benigno <gbenignorocha@gmail.com>
2022-08-16 17:19:37 -0300

    Corrigindo a classe atom

--- lib/atom.dart
+++ lib/atom.dart
@@ -6,13 +6,19 @@ class Atom{
   final List<Element> _data = Elements().elementsList;
 
   Atom(String? symbolElement){
-    if (_data.contains(symbolElement)) {
-      _symbol = symbolElement;
+    if (symbolElement != null) {
+      _data.forEach((element){
+        if (element.symbol == symbolElement) {
+          _symbol = symbolElement;
+        }
+    });
+      throw Exception('Invalid Element Symbol');
     }
     else{
-      throw Exception('Invalid Symbol');
+        throw Exception('Null Value');
+      }
     }
-  }
+    
   @override
   String toString() => _symbol ??= 'null';
 }
