04c31c7ece6beea5aeaa8da084444dd1920e339e
Gabriel Benigno <gbenignorocha@gmail.com>
2022-08-16 13:57:31 -0300

    Quase finalizando o construtor da classe molecule

--- lib/molecule.dart
+++ lib/molecule.dart
@@ -2,11 +2,38 @@
 class Molecule implements Comparable{
   
   final List<Map<String, int>> _moleculeFormula = [];
-  final String description;
+  late final String _description;
 
-  Molecule({required String formula, required String name}){
-    var regex = RegExp('([A-Z][a-z]?)([0-9]*)');
-  
+  Molecule({required String? formula, required String name}){
+    _description = name;
+    if (formula == null) {
+      throw Exception('Null Value');
+    }
+    else{
+    final regex = RegExp('([A-Z][a-z]?)([0-9]*)');
+    final splitFormula = regex.firstMatch(formula);
+      if (splitFormula == null) {
+        throw Exception('Invalid Formula Format');
+      }
+      else{
+        final quantityDivisons = regex.allMatches(formula);
+        for (var i = 1; i < quantityDivisons.length; i++) {
+        if (_moleculeFormula[i - 1].containsKey(splitFormula.group(i).toString())) {
+          _moleculeFormula[i - 1].update(splitFormula.group(i).toString(), (value) => value + 1);
+        }
+        else{
+          if (splitFormula.group(i + 1).toString() == 'null') {
+            _moleculeFormula[i - 1].putIfAbsent(splitFormula.group(i).toString(), () => 1);
+          }
+          else if(splitFormula.group(i + 1).toString() == '1'){
+            throw Exception('Invalid Formula');
+          }
+          else{_moleculeFormula[i - 1].putIfAbsent(splitFormula.group(i).toString(), () => int.parse(splitFormula.group(i + 1).toString()));
+          }
+        }
+        }
+      }
+    }
   }
 
   get formula => ;
